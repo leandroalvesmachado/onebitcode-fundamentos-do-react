41ce012e8c6e3d7a42d44b71d21399ade5214581
leandroalvesmachado <leandroalvesmachado@gmail.com>
2023-06-20 22:35:14 -0300

    salvando

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 
 # Composição de componentes com “children”
 
+# Props com Rest Param
+- Passar props do componente pai para o filho
+<ProfileSection
+    className={styles.links}
+    id="links-section"
+    data-test="some value"
+    aria-label="social links"
+>
+
+<div
+      {...props}
+      className={styles.wrapper}
+    >
+      {props.children}
+    </div>
+
+<div
+      {...props}
+      className={`${styles.wrapper} ${props.className}`}
+    >
+      {props.children}
+    </div>
--- exercicio-03/src/components/Profile/index.jsx
+++ exercicio-03/src/components/Profile/index.jsx
@@ -3,6 +3,13 @@ import Title from "../Title"
 import ProfileSection from "./ProfileSection"
 import styles from "./styles.module.css"
 
+// Se a função não depende de nenhuma props, fica aqui fora
+// Se a função depender de alguma props, fica dentro do componente. Ex: export default function Profile(props) {
+function handleClick(ev) {
+  console.log(ev)
+  alert('Você agora está seguindo')
+}
+
 export default function Profile(props) {
   return (
     <div className={styles.container}>
@@ -10,18 +17,28 @@ export default function Profile(props) {
       <Title>
         <span>{props.name}</span>
         <p>
-          <button>Componente com children</button>
+          {/* eventos do js no react */}
+          {/* <button onClick={() => alert('Você agora esta seguindo')}>Follow</button> */}
+          <button
+            className={styles.followButton}
+            onClick={handleClick}
+          >
+            Follow
+          </button>
         </p>
       </Title>
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.phone}</ProfileSection>
       <ProfileSection>{props.email}</ProfileSection>
-      <ProfileSection>
-        <div className={styles.links}>
-          <LinkButton href={props.github} target="_blank">GitHub</LinkButton>
-          <LinkButton href={props.linkedin} target="_blank">Linkedin</LinkButton>
-          <LinkButton href={props.twitter} target="_blank">Twitter</LinkButton>
-        </div>
+      <ProfileSection
+        className={styles.links}
+        id="links-section"
+        data-test="some value"
+        aria-label="social links"
+      >
+        <LinkButton href={props.github} target="_blank">GitHub</LinkButton>
+        <LinkButton href={props.linkedin} target="_blank">Linkedin</LinkButton>
+        <LinkButton href={props.twitter} target="_blank">Twitter</LinkButton>
       </ProfileSection>
     </div>
   )
